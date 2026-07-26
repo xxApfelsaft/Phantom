8c778e9772f4397c8a31fb43063c929a3091d3d4
xxApfelsaft <finnschafer222@gmail.com>
2026-07-26 06:55:24 +0200

    feat: added a Active Modules Hud Element

--- src/main/java/dev/xxapfelsaft/phantom/PhantomAddon.java
+++ src/main/java/dev/xxapfelsaft/phantom/PhantomAddon.java
@@ -6,6 +6,7 @@ import dev.xxapfelsaft.phantom.feature.modules.HitSoundModule;
 import dev.xxapfelsaft.phantom.feature.modules.CustomNameTagModule;
 import dev.xxapfelsaft.phantom.feature.modules.BridgeAssistModule;
 import dev.xxapfelsaft.phantom.feature.modules.ServerIPElement;
+import dev.xxapfelsaft.phantom.feature.modules.ActiveModulesElement;
 import com.dwarslooper.cactus.client.addon.v2.ICactusAddon;
 import com.dwarslooper.cactus.client.addon.v2.RegistryBus;
 import com.dwarslooper.cactus.client.feature.module.Category;
@@ -33,6 +34,7 @@ public class PhantomAddon implements ICactusAddon {
         registryBus.register(Module.class, ctx -> new CustomNameTagModule());
         registryBus.register(Module.class, ctx -> new BridgeAssistModule());
         registryBus.register(HudElement.class, ctx -> new ServerIPElement());
+        registryBus.register(HudElement.class, ctx -> new ActiveModulesElement());
 
         LOGGER.info("Phantom Addon registered successfully!");
     }
